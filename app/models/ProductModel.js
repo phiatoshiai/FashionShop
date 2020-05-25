3bcab964a84d154fbef1e68094f39af92b877e46
phipham.qn93 <phipham.qn93@gmail.com>
2020-05-25 20:32:17 +0700

    new model

---
+++ app/models/ProductModel.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var productSchema = new Schema({
+  name: { type: String },
+  color: { type: String },
+  material: { type: String },
+  code: { type: String },
+  origin: { type: String },
+  pictureUrl: [{ type: String }],
+  // Filtering
+  activated: { type: Boolean, default: true },
+  deleted: { type: Boolean, default: false },
+  slug: { type: String },
+  // Auditing
+  createdBy: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  updatedBy: { type: String },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('products', productSchema);
